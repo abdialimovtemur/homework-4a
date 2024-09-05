c00c650b871461332e42b28d609a34481a4bf37e
abdialimovtemur <abdialimovtemur@gmail.com>
2024-09-05 11:56:27 +0500

    Commit title

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-const open__modal = document.querySelector('.open__modal')
-const drower = document.querySelector('.drower_dr')
+const drower_btn = document.querySelector('.open__modal')
+const drower = document.querySelector('.drower__dr')
 const drower__content = document.querySelector('.drower__content')
+const drower__close = document.querySelector('.drower__close')
+const close__button = document.querySelector('.close__button')
+const modal__btn = document.querySelector('.open__modal2')
+const modal__content = document.querySelector('.modal__content')
 
+const removeDrover = () => {
+    drower__content.classList.remove("open");
+    drower.classList.remove("show");
+}; 
 
-open__modal.addEventListener('click',()=>{
-    console.log( )
+drower_btn.addEventListener('click',()=>{
+    drower__content.classList.toggle("open");
+    drower.classList.add("show");
+});
+
+modal__btn.addEventListener('click' ,()=> {
+    modal__content.style.opacity = "1";
 })
+
+close__button.addEventListener('click', ()=>{
+    modal__content.style.opacity = "0";
+
+})
+
+
+drower.addEventListener("click", removeDrover);
