a55a51668840f3412fb6acbe5e48e52b0cf4ca70
abdialimovtemur <abdialimovtemur@gmail.com>
2024-09-05 07:32:37 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const open__modal = document.querySelector('.open__modal')
+const drower = document.querySelector('.drower_dr')
+const drower__content = document.querySelector('.drower__content')
+
+
+open__modal.addEventListener('click',()=>{
+    console.log( )
+})
